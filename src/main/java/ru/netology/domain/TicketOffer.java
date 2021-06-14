a5f89a89b39127fa513e83c275449c6f141a43bf
yannikkel <78969123+yannikkel@users.noreply.github.com>
2021-06-14 11:44:53 +0300

    domain

---
+++ src/main/java/ru/netology/domain/TicketOffer.java
@@ -0,0 +1,21 @@
+package domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class TicketOffer implements Comparable<TicketOffer> {
+    private int id;
+    private int price;
+    private String airportDeparture;
+    private String airportArrival;
+    private int flightTimeInMinutes;
+
+    @Override
+    public int compareTo(TicketOffer o) {
+        return this.price - o.price;
+    }
